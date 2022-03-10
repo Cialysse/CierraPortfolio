0fde74567b908b596a189f48ea0d01956edd44f8
Cialysse <marshall.cierra714@gmail.com>
2022-03-10 08:55:25 -0500

    fixed font colors and sizes/first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// Background Change Header
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// Works Section
+let mixerPortfolio = mixitup('.work-container', {
+    selectors: {
+        target: '.work-card',
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+// Link active work
+const linkWork = document.querySelectorAll('.work-item')
+
+function activeWork() {
+    linkWork.forEach( l => l.classList.remove('.active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener('click', activeWork));
+
+// Scrollbar Section
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// Scroll Animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+})
+
+sr.reveal(`.home-data`)
+sr.reveal(`.home-handle`, {delay: 700})
+sr.reveal(`.home-social`, {delay: 900, origin: 'bottom'})
